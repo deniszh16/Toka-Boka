64375633a104d0b8c998d303008f05214fc776d8
Denis Zhursky <zhursky16@gmail.com>
2023-10-26 15:47:27 +0300

    Кнопка открытия всех игр

---
+++ Assets/Scripts/Logic/UI/Buttons/OpeningLink.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Logic.UI.Buttons
+{
+    [RequireComponent(typeof(Button))]
+    public class OpeningLink : MonoBehaviour
+    {
+        private Button _button;
+        private const string YandexGames = "https://yandex.ru/games/developer?name=DZ%20Games";
+
+        private void Awake() =>
+            _button = GetComponent<Button>();
+
+        private void OnEnable() =>
+            _button.onClick.AddListener(OpenDeveloperPage);
+
+        private void OpenDeveloperPage() =>
+            Application.OpenURL(YandexGames);
+
+        private void OnDisable() =>
+            _button.onClick.RemoveListener(OpenDeveloperPage);
+    }
+}
